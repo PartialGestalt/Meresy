576477cbdcbe0ea7ca6501d9c0188a3a359ab790
Andrew Kephart <andrew.kephart@alereon.com>
2017-04-19 16:28:58 -0500

    development snapshot

--- index.js
+++ index.js
@@ -10,6 +10,7 @@
  */
 var MeresyNumber = require('./lib/MeresyNumber');
 var Httpd = require('./lib/MeresyHttpd');
+var Url = require('url');
 
 /**
  * Add a handler for quitting.
@@ -25,7 +26,17 @@ Httpd.addHandler('get','/quit',function(q,r) {
 
 process.on('exit',()=> {
     console.log("Exiting.");
-})
+});
+
+Httpd.addHandler('get','/numencode', (q,r) => {
+    var input = Url.parse(q.url);
+    console.log("You asked for: " + input.query);
+    r.writeHead(200,{'Content-Type': 'text/plain'});
+    r.write('NumEncode: You asked for ' + input.query + '\n');
+    var encoded = MeresyNumber.encode(input.query)
+    r.write('Encoded to: ' + encoded + '\n');
+    r.end('Decoded back to: ' + MeresyNumber.decode(encoded));
+});
 
 /**
  * Business end of the stick
